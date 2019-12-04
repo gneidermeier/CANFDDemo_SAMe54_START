e3aba06f194bedef1d3066cad1c66c2cd0c69c94
U-LOGFWA\gneidermeier <gneidermeier@FWA002506.logikos.com>
2019-12-04 08:56:04 -0500

    Import CAN FD Demo for SAMe54 from start.atmel.com

---
+++ CANFDDemo/driver_init.c
@@ -0,0 +1,81 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+
+struct can_async_descriptor CAN_0;
+
+struct usart_sync_descriptor TARGET_IO;
+
+void TARGET_IO_PORT_init(void)
+{
+
+	gpio_set_pin_function(PB25, PINMUX_PB25D_SERCOM2_PAD0);
+
+	gpio_set_pin_function(PB24, PINMUX_PB24D_SERCOM2_PAD1);
+}
+
+void TARGET_IO_CLOCK_init(void)
+{
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM2_GCLK_ID_CORE, CONF_GCLK_SERCOM2_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM2_GCLK_ID_SLOW, CONF_GCLK_SERCOM2_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+
+	hri_mclk_set_APBBMASK_SERCOM2_bit(MCLK);
+}
+
+void TARGET_IO_init(void)
+{
+	TARGET_IO_CLOCK_init();
+	usart_sync_init(&TARGET_IO, SERCOM2, (void *)NULL);
+	TARGET_IO_PORT_init();
+}
+
+void CAN_0_PORT_init(void)
+{
+
+	gpio_set_pin_function(PB13, PINMUX_PB13H_CAN1_RX);
+
+	gpio_set_pin_function(PB12, PINMUX_PB12H_CAN1_TX);
+}
+/**
+ * \brief CAN initialization function
+ *
+ * Enables CAN peripheral, clocks and initializes CAN driver
+ */
+void CAN_0_init(void)
+{
+	hri_mclk_set_AHBMASK_CAN1_bit(MCLK);
+	hri_gclk_write_PCHCTRL_reg(GCLK, CAN1_GCLK_ID, CONF_GCLK_CAN1_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	can_async_init(&CAN_0, CAN1);
+	CAN_0_PORT_init();
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PC13
+
+	gpio_set_pin_level(CAN_STDBY,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(CAN_STDBY, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(CAN_STDBY, GPIO_PIN_FUNCTION_OFF);
+
+	TARGET_IO_init();
+	CAN_0_init();
+}
---
+++ CANFDDemo/main.c
@@ -0,0 +1,239 @@
+/**
+ * \file
+ *
+ * \brief CAN FD Example.
+ *
+ * Copyright (c) 2019 Microchip Technology Inc. and its subsidiaries.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Subject to your compliance with these terms, you may use Microchip
+ * software and any derivatives exclusively with Microchip products.
+ * It is your responsibility to comply with third party license terms applicable
+ * to your use of third party software (including open source software) that
+ * may accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE,
+ * INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY,
+ * AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE
+ * LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
+ * SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
+ * POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
+ * RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+ * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * \asf_license_stop
+ *
+ */
+
+#include <atmel_start.h>
+
+/**
+ * \brief display configuration menu.
+ */
+static void display_menu(void)
+{
+	printf("Menu :\r\n"
+	       "  -- Select the action:\r\n"
+	       "  0: Set standard filter ID 0: 0x45A, store into Rx buffer. \r\n"
+	       "  1: Set standard filter ID 1: 0x469, store into Rx FIFO 0. \r\n"
+	       "  2: Send FD standard message with ID: 0x45A and 64 byte data 0 to 63. \r\n"
+	       "  3: Send FD standard message with ID: 0x469 and 64 byte data 128 to 191. \r\n"
+	       "  4: Set extended filter ID 0: 0x100000A5, store into Rx buffer. \r\n"
+	       "  5: Set extended filter ID 1: 0x10000096, store into Rx FIFO 1. \r\n"
+	       "  6: Send FD extended message with ID: 0x100000A5 and 64 byte data 0 to 63. \r\n"
+	       "  7: Send FD extended message with ID: 0x10000096 and 64 byte data 128 to 191. \r\n"
+	       "  a: Send normal standard message with ID: 0x469 and 8 byte data 0 to 7. \r\n"
+	       "  h: Display menu \r\n\r\n");
+}
+
+static void CAN_0_tx_callback(struct can_async_descriptor *const descr)
+{
+	(void)descr;
+	printf("  CAN Transmission done \r\n");
+}
+
+static void CAN_std_tx_callback(struct can_async_descriptor *const descr)
+{
+	(void)descr;
+
+	/* Enable the FDOE and BRSE in register configuration*/
+	hri_can_set_CCCR_INIT_bit(CAN_0.dev.hw);
+	while (hri_can_get_CCCR_INIT_bit(CAN_0.dev.hw) == 0)
+		;
+	hri_can_set_CCCR_CCE_bit(CAN_0.dev.hw);
+
+	hri_can_set_CCCR_FDOE_bit(CAN_0.dev.hw);
+	hri_can_set_CCCR_BRSE_bit(CAN_0.dev.hw);
+
+	printf("  CAN Transmission done \r\n");
+}
+
+static void CAN_0_rx_callback(struct can_async_descriptor *const descr)
+{
+	struct can_message msg;
+	uint8_t            data[64];
+	msg.data = data;
+	can_async_read(descr, &msg);
+
+	printf("\n\r CAN Message received . The received data is: \r\n");
+	for (uint8_t i = 0; i < msg.len; i++) {
+		printf("  %d", msg.data[i]);
+	}
+	printf("\r\n\r\n");
+	return;
+}
+
+int main(void)
+{
+	uint8_t key;
+
+	struct can_message msg;
+	struct can_filter  filter;
+
+	uint8_t tx_message_0[64];
+	uint8_t tx_message_1[64];
+	uint8_t tx_message_2[8];
+
+	/* Initializes MCU, drivers and middleware */
+	atmel_start_init();
+	display_menu();
+
+	/* Initialize the data to be used later */
+	for (uint8_t i = 0; i < 64; i++) {
+		tx_message_0[i] = i;
+	}
+
+	for (uint8_t j = 128; j < 192; j++) {
+		tx_message_1[j - 128] = j;
+	}
+
+	for (uint8_t k = 0; k < 8; k++) {
+		tx_message_2[k] = k;
+	}
+
+	while (1) {
+		scanf("%c", (char *)&key);
+
+		switch (key) {
+		case 'h':
+			display_menu();
+			break;
+
+		case '0':
+			printf("  0: Set standard filter ID 0: 0x45A, store into Rx buffer. \r\n");
+			can_async_register_callback(&CAN_0, CAN_ASYNC_RX_CB, (FUNC_PTR)CAN_0_rx_callback);
+			filter.id   = 0x45A;
+			filter.mask = 0;
+			can_async_set_filter(&CAN_0, 0, CAN_FMT_STDID, &filter);
+			break;
+
+		case '1':
+			printf("  1: Set standard filter ID 1: 0x469, store into Rx FIFO 0. \r\n");
+			can_async_register_callback(&CAN_0, CAN_ASYNC_RX_CB, (FUNC_PTR)CAN_0_rx_callback);
+			filter.id   = 0x469;
+			filter.mask = 0;
+			can_async_set_filter(&CAN_0, 1, CAN_FMT_STDID, &filter);
+			break;
+
+		case '2':
+			printf("  2: Send standard message with ID: 0x45A and 64 byte data 0 to 63. \r\n");
+			msg.id   = 0x45A;
+			msg.type = CAN_TYPE_DATA;
+			msg.data = tx_message_0;
+			msg.len  = 64;
+			msg.fmt  = CAN_FMT_STDID;
+
+			can_async_register_callback(&CAN_0, CAN_ASYNC_TX_CB, (FUNC_PTR)CAN_0_tx_callback);
+			can_async_enable(&CAN_0);
+			can_async_write(&CAN_0, &msg);
+			break;
+
+		case '3':
+			printf("  3: Send standard message with ID: 0x469 and 64 byte data 128 to 191. \r\n");
+			msg.id   = 0x469;
+			msg.type = CAN_TYPE_DATA;
+			msg.data = tx_message_1;
+			msg.len  = 64;
+			msg.fmt  = CAN_FMT_STDID;
+
+			can_async_register_callback(&CAN_0, CAN_ASYNC_TX_CB, (FUNC_PTR)CAN_0_tx_callback);
+			can_async_enable(&CAN_0);
+			can_async_write(&CAN_0, &msg);
+			break;
+
+		case '4':
+			printf("  4: Set extended filter ID 0: 0x100000A5, store into Rx buffer. \r\n");
+			can_async_register_callback(&CAN_0, CAN_ASYNC_RX_CB, (FUNC_PTR)CAN_0_rx_callback);
+			filter.id   = 0x100000A5;
+			filter.mask = 0;
+			can_async_set_filter(&CAN_0, 0, CAN_FMT_EXTID, &filter);
+			break;
+
+		case '5':
+			printf("  5: Set extended filter ID 1: 0x10000096, store into Rx FIFO 1. \r\n");
+			can_async_register_callback(&CAN_0, CAN_ASYNC_RX_CB, (FUNC_PTR)CAN_0_rx_callback);
+			filter.id   = 0x10000096;
+			filter.mask = 0;
+			can_async_set_filter(&CAN_0, 1, CAN_FMT_EXTID, &filter);
+			break;
+
+		case '6':
+			printf("  6: Send extended message with ID: 0x100000A5 and 64 byte data 0 to 63. \r\n");
+			msg.id   = 0x100000A5;
+			msg.type = CAN_TYPE_DATA;
+			msg.data = tx_message_0;
+			msg.len  = 64;
+			msg.fmt  = CAN_FMT_EXTID;
+
+			can_async_register_callback(&CAN_0, CAN_ASYNC_TX_CB, (FUNC_PTR)CAN_0_tx_callback);
+			can_async_enable(&CAN_0);
+			can_async_write(&CAN_0, &msg);
+			break;
+
+		case '7':
+			printf("  7: Send extended message with ID: 0x10000096 and 64 byte data 128 to 191. \r\n");
+			msg.id   = 0x10000096;
+			msg.type = CAN_TYPE_DATA;
+			msg.data = tx_message_1;
+			msg.len  = 64;
+			msg.fmt  = CAN_FMT_EXTID;
+
+			can_async_register_callback(&CAN_0, CAN_ASYNC_TX_CB, (FUNC_PTR)CAN_0_tx_callback);
+			can_async_enable(&CAN_0);
+			can_async_write(&CAN_0, &msg);
+			break;
+
+		case 'a':
+			printf("  a: Send normal standard message with ID: 0x469 and 8 byte data 0 to 7. \r\n");
+			msg.id   = 0x469;
+			msg.type = CAN_TYPE_DATA;
+			msg.data = tx_message_2;
+			msg.len  = 8;
+			msg.fmt  = CAN_FMT_STDID;
+
+			/* Disable the FDOE and BRSE from register configuration
+			 * and enable them again in callback */
+			hri_can_set_CCCR_INIT_bit(CAN_0.dev.hw);
+			while (hri_can_get_CCCR_INIT_bit(CAN_0.dev.hw) == 0)
+				;
+			hri_can_set_CCCR_CCE_bit(CAN_0.dev.hw);
+
+			hri_can_clear_CCCR_FDOE_bit(CAN_0.dev.hw);
+			hri_can_clear_CCCR_BRSE_bit(CAN_0.dev.hw);
+
+			can_async_register_callback(&CAN_0, CAN_ASYNC_TX_CB, (FUNC_PTR)CAN_std_tx_callback);
+			can_async_enable(&CAN_0);
+			can_async_write(&CAN_0, &msg);
+			break;
+
+		default:
+			break;
+		}
+	}
+}
